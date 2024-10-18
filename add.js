71ae66194a1e0057974b84bd5f5ac827c8e9ae67
2300032615 <143894454+2300032615@users.noreply.github.com>
2024-10-18 12:54:56 +0530

    2300032615_BADIGA AASHRITHA

---
+++ add.js
@@ -0,0 +1,4 @@
+let name = "AASHRITHA";
+let id = 2300032615;
+console.log("Name: " + name);
+console.log("ID: " + id);
---
+++ hellow.js
@@ -0,0 +1,4 @@
+console.log("welcome to zero one");
+let arr =[1,2,3,4];
+let newar= arr.map((x)=>x*x);
+console.log(newar)
